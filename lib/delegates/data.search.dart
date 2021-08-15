fee8a6dceea21dae46dd29355b1dae68d6d92eae
guilhermow <gui.crb0@gmail.com>
2021-08-15 12:02:13 -0300

    finalizando o sistema de buscas

--- lib/delegates/data.search.dart
+++ lib/delegates/data.search.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,9 +14,7 @@ class DataSearch extends SearchDelegate<String> {
 
       inputDecorationTheme: InputDecorationTheme(
         border: InputBorder.none,
-
       ),
-
       textTheme: TextTheme(
         bodyText2: TextStyle(color: Colors.white),
       ),
@@ -42,24 +42,58 @@ class DataSearch extends SearchDelegate<String> {
         color: Colors.grey,
       ),
       onPressed: () {
-        close(context, '');
+        close(context, ''); 
       },
     );
   }
 
   @override
   Widget buildResults(BuildContext context) {
+    Future.delayed(Duration.zero).then((_)=>close(context, query));
     return Container();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Container();
+    if(query.isEmpty) return Container();
+    else return FutureBuilder<List>(
+      future: suggestions(query),
+      builder: (context, snapshot){
+        if(!snapshot.hasData){
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }else{
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index){
+              return ListTile(
+                title: Text(snapshot.data![index] ?? '######### not found ##########' ),
+                leading: Icon(Icons.play_arrow),
+                onTap: (){
+                  close(context, snapshot.data![index]);
+                },
+              );
+            },
+          );
+        }
+      },
+    );
   }
 
-  suggestions(String search) async {
+  Future<List> suggestions(String search) async {
     http.Response response = await http.get(
       Uri.parse("http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json")
     );
+    
+    if(sucessfullResponse(response)){
+      return json.decode(response.body)[1].map((v){
+        return v[0];
+      }).toList();
+    } else {
+      throw Exception('failed to load suggestions');
+    }
   }
+
+  bool sucessfullResponse(http.Response response) => response.statusCode == 200;
 }
